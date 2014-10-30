2452760dd774092586af241d33c8624a84a22a80
Dimas Cyriaco <dimascyriaco@gmail.com>
2014-10-30 15:34:14 -0200

    Extraindo lógica para o método Camara::Deputados::Client#clean_xml

--- lib/camara/deputados/client.rb
+++ lib/camara/deputados/client.rb
@@ -1,13 +1,19 @@
 module Camara::Deputados::Client
-  def obter_deputado(id)
-    response = Camara.connection.get "/SitCamaraWS/Deputados.asmx/ObterDetalhesDeputado?ideCadastro=#{id}&numLegislatura="
-    data = Nokogiri::XML response.body
-    Camara::Deputados::Deputado.new data.xpath('//Deputado')
-  end
-
   def obter_deputados
     response = Camara.connection.get "/SitCamaraWS/Deputados.asmx/ObterDeputados"
-    data = Nokogiri::XML response.body
-    data.xpath('//deputado').map { |deputado| Camara::Deputados::Deputado.new(deputado) }
+    data = Nokogiri::XML clean_xml(response.body)
+    data.css('deputado').map { |deputado| Camara::Deputados::Deputado.new(deputado) }
   end
+
+  def obter_deputado(id)
+    response = Camara.connection.get "/SitCamaraWS/Deputados.asmx/ObterDetalhesDeputado?ideCadastro=#{id}&numLegislatura="
+
+    data = Nokogiri::XML clean_xml(response.body)
+    Camara::Deputados::Deputado.new data.at_css('Deputado')
+  end
+
+  private
+    def clean_xml(xml)
+      xml.gsub("\r\n", '').squeeze(' ')
+    end
 end
