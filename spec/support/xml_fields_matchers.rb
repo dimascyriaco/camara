bcb0618d96b598408a73101dc523d4ca48e82471
Dimas Cyriaco <dimascyriaco@gmail.com>
2014-11-01 16:24:37 -0200

    Adicionado matcher 'have_field'

--- spec/support/xml_fields_matchers.rb
+++ spec/support/xml_fields_matchers.rb
@@ -3,3 +3,14 @@ RSpec::Matchers.define :have_fields do |expected|
     actual.fields.include? *expected
   end
 end
+
+RSpec::Matchers.define :have_field do |expected, options|
+  match do |actual|
+    field = nil
+    actual.fields.find do |f|
+      key = f.is_a?(Hash) && f.keys.find {|k| k == expected }
+      field = f[key] if key
+    end
+    field && field == options[:as_class]
+  end
+end
