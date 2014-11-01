89e3343fd808b266d7d72aa3c56bcb78c88f865d
Dimas Cyriaco <dimascyriaco@gmail.com>
2014-11-01 17:54:22 -0200

    Removido log do Faraday

--- lib/camara/connection.rb
+++ lib/camara/connection.rb
@@ -4,7 +4,6 @@ module Camara::Connection
   def default_options
     {
       request: :url_encoded,
-      response: :logger,
       adapter: Faraday.default_adapter
     }
   end
@@ -12,7 +11,6 @@ module Camara::Connection
   def init
     @connection = Faraday.new(url: 'http://www.camara.gov.br') do |faraday|
       faraday.request  default_options[:request]
-      faraday.response default_options[:response]
       faraday.adapter  default_options[:adapter]
     end
   end
