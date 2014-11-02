6e07d9794b940850edf1444c66d641952b82fff5
Dimas Cyriaco <dimascyriaco@gmail.com>
2014-11-01 22:44:29 -0200

    Nova singularazação

--- lib/camara/initialize.rb
+++ lib/camara/initialize.rb
@@ -4,4 +4,5 @@ ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.singular 'comissoes', 'comissao'
   inflect.singular 'periodosExercicio', 'periodoExercicio'
   inflect.singular 'cargosComissoes', 'cargoComissoes'
+  inflect.singular 'lideres', 'lider'
 end
